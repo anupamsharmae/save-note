9f16e625602b2ea06f6da5668d284bd8532b6315
Anupam Sharma <mr.anupamkrsharma@gmail.com>
2021-12-13 23:36:07 +0530

    update commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
 const root = document.getElementById('root')
-const addNotebtn = document.getElementById('addNotebtn')
 const listSection = document.getElementById('listSection')
 const editSection = document.getElementById('editSection')
+const currentTime = document.getElementById('currentTime')
 const textBox = document.getElementById('textBox')
 const textArea = document.getElementById('textArea')
 const saveBtn = document.getElementById('saveBtn')
+const closeBtn = document.getElementById('closeBtn')
+const addNotebtn = document.getElementById('addnote')
+
 const listArray = []
 let id = 0
 let currentId = 0
 
+function updateTime(){
+    let date = new Date()
+    let dateNow = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear()
+    let timeNow = date.getHours() + ':' + date.getMinutes()
+    return (dateNow + ' / ' + timeNow)
+}
 
-saveBtn.addEventListener('click', (e) => {
-    
-    if (!textArea.value == '' || !textBox.value =='') {
+
+saveBtn.addEventListener('click', () => {
+
+    if (!textArea.value == '' || !textBox.value == '') {
 
         if (currentId != 0) {
             let currentObj = new CardList(textBox.value, textArea.value, currentId)
             listArray[currentId] = currentObj
-            listSection.replaceChild(currentObj, listSection.childNodes[currentId])
+            listSection.replaceChild(currentObj, listSection.children[currentId-1])
             reset()
         } else {
             id = id + 1
@@ -27,6 +37,7 @@ saveBtn.addEventListener('click', (e) => {
 
             //console.log(listArray.at(-1))
             listSection.appendChild(obj)
+
             reset()
         }
 
@@ -37,50 +48,99 @@ saveBtn.addEventListener('click', (e) => {
 })
 
 
+function closePopup() {
+    reset()
+    editSection.style.display = 'none'
+    addNotebtn.style.display = ''
+}
+
+function openPopup() {
+    currentTime.innerText = updateTime()
+    editSection.style.display = 'block'
+    addNotebtn.style.display = 'none'
+}
+
+closeBtn.addEventListener('click', closePopup)
+addNotebtn.addEventListener('click', openPopup)
+
 function update(heading, content, id) {
     textBox.value = heading
     textArea.value = content
     currentId = id
+    openPopup()
 }
 
-function reset(){
+function reset() {
     textBox.value = ''
     textArea.value = ''
     currentId = 0
+}
+
+function resetCardID(){
+    let divCard = document.querySelectorAll('.card')
+        for(let i=0; i< divCard.length; i++)
+        {
+            divCard[i].setAttribute('id', i+1)        
+        }
+        if(divCard.length == 0){
+            id = 0
+        }
 }
 
 class CardList {
     constructor(heading, text, id) {
         this.div = document.createElement('div')
         this.title = document.createElement('h1')
-        this.closeBtn = document.createElement('span')
-        
+        this.bannerCard = document.createElement('div')
+        this.dateTime = document.createElement('span')
+        this.discardBtn = document.createElement('button')
+        this.openBtn = document.createElement('button')
+
         this.li = document.createElement('li')
         this.div.setAttribute('id', id)
         this.div.setAttribute('class', 'card')
-        this.div.setAttribute('tabindex','-1')
-        this.closeBtn.setAttribute('class','closeBtn')
+        this.div.setAttribute('tabindex', '-1')
+        this.bannerCard.setAttribute('class', 'banner')
+        this.dateTime.setAttribute('id', 'dateTime')
+        this.openBtn.setAttribute('id','openCard')
+        this.discardBtn.setAttribute('id', 'discard')
+
         this.title.innerText = heading
         this.li.innerText = text
-        this.closeBtn.innerText = 'X'
+       this.dateTime.innerText = updateTime()
+       this.openBtn.innerText = 'Open'
+       this.discardBtn.innerText = 'Discard'
 
-        this.div.addEventListener('focusin', () => this.listCard())
-        this.div.addEventListener('focusout',()=> this.focusOutMethod())
-        this.closeBtn.addEventListener('click', ()=>this.close())
+        this.div.addEventListener('dblclick', () => this.listCard())
+        this.openBtn.addEventListener('click', () => this.listCard())
+        
+        //this.div.addEventListener('focusout', () => this.focusOutMethod())
+        //this.closeCard.addEventListener('click', () => this.close())
+        this.discardBtn.addEventListener('click', () => this.close())
+        
+
+        this.bannerCard.appendChild(this.dateTime)
+        this.bannerCard.appendChild(this.openBtn)
+        this.bannerCard.appendChild(this.discardBtn)
         this.div.appendChild(this.title)
-        this.div.appendChild(this.closeBtn)
+       // this.div.appendChild(this.closeCard)
         this.div.appendChild(this.li)
+        this.div.appendChild(this.bannerCard)
         return this.div
     }
-
+    Card(id){
+        update(this.title.innerText, this.li.innerText, id)
+        console.log(id)
+    }
     listCard() {
         update(this.title.innerText, this.li.innerText, this.div.id)
     }
-    focusOutMethod(){ 
+    focusOutMethod() {
         //reset()
     }
-    close(){
-        reset()
+    close() {
+        reset()  
         this.div.remove()
+        resetCardID()
     }
 }
